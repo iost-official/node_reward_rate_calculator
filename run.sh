58c72580350df1251b043d325dbf9705c73216de
张卓 <mycinbrin@gmail.com>
2020-12-15 18:58:59 +0800

    rename tp english

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-node 实际.js > 实际.csv
-node 理论.js > votes.go
-go run ./... | tail -n +2 | tac > 理论.csv
+node real.js > real.csv
+node theory.js > votes.go
+go run ./... | tail -n +2 | tac > theory.csv
 zip vote_return_`date +%Y%m%d`.zip *.csv
